30c985d4be329435c6ee5bc623ca1e8208070ca1
Holger Müller <holger.mueller@eulg.de>
2016-07-28 19:06:09 +0200

    EULG-8630: Teil 2 - Material im Setup

--- Setup/Setup/App.xaml.cs
+++ Setup/Setup/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using Eulg.Shared;
 
@@ -9,6 +10,24 @@ namespace Eulg.Setup
 {
     public partial class App
     {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Margins
+        {
+            public int LeftWidth;
+            public int RightWidth;
+            public int TopHeight;
+            public int BottomHeight;
+        }
+
+        #region Dll Imports
+
+        [DllImport("dwmapi.dll", PreserveSig = true)]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins pMarInset);
+        #endregion
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             /* 
---
+++ Setup/Setup/WindowTools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace Eulg.Setup
+{
+    public static class WindowTools
+    {
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        public static void EnableBorderlessWindowDragging(Window window)
+        {
+            MouseButtonEventHandler mouseEvent = delegate
+            {
+                ReleaseCapture();
+                SendMessage(new WindowInteropHelper(window).Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            };
+
+            window.MouseLeftButtonDown += mouseEvent;
+        }
+    }
+}
