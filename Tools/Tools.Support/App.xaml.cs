1eb96e84aaf7eec8f24b86248e177913f33637a4
Florian Wild <florian.wild@ks-software.de>
2016-07-28 16:40:34 +0200

    EULG-8853: Support-Tool Fensterschatten EULG-8854: Support-Tool Feste Größe für Fenster

--- Tools/Tools.Support/App.xaml.cs
+++ Tools/Tools.Support/App.xaml.cs
@@ -1,8 +1,27 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace Eulg.Client.SupportTool
 {
     public partial class App : Application
     {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Margins
+        {
+            public int LeftWidth;
+            public int RightWidth;
+            public int TopHeight;
+            public int BottomHeight;
+        }
+
+        #region Dll Imports
+
+        [DllImport("dwmapi.dll", PreserveSig = true)]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        [DllImport("dwmapi.dll")]
+        public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Margins pMarInset);
+        #endregion
     }
 }
