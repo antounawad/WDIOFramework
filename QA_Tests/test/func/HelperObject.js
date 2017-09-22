e92d55f4c56616064a97484da851d2620fdf7ed2
antoun.awad <antoun.awad@xbav.de>
2017-09-22 11:21:00 +0200

    Click und Search Funktionen eingebaut

--- QA_Tests/test/func/HelperObject.js
+++ QA_Tests/test/func/HelperObject.js
@@ -3,7 +3,20 @@ var Helper =(function(){
     function Helper() {
         this.url = process.argv[3];
         this.targetUrl = this.url.substr(12);
+
+
+        function ClickAction(selector, timeout){
+            var retValue = $(selector);
+            assert.notEqual(retValue.selector,"");
+            browser.waitForEnabled(retValue.selector, timeout);
+            browser.click(retValue.selector);
+            console.log(browser.getTitle());
+        };
+
+
     };
+
+    
     
     return Helper;
     
