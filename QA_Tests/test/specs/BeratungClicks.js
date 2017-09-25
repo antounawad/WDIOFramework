c52ac788fe88ff8fb56d42828ca11e931e7d0136
antoun.awad <antoun.awad@xbav.de>
2017-09-25 13:21:08 +0200

    Timer anpassen

--- QA_Tests/test/specs/BeratungClicks.js
+++ QA_Tests/test/specs/BeratungClicks.js
@@ -117,7 +117,7 @@ describe('webdriver.io page', function () {
 			browser.pause(8000);
 			ClickAction('#navChapterLink_6', 100000, 8000);
 			ClickAction('#navViewLink_AngebotAngebotAngebotsdaten', 100000, 8000);
-			ClickAction('#btnNavNext', 100000, 30000);
+			ClickAction('#btnNavNext', 100000, 15000);
 
 			// var retValue = $('.md-title ng-binding');
 			// assert.equal(retValue,null,'Fehler bei der Angebotserstellung.');
