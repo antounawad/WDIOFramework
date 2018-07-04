4984cfd7237ce8b75575660a57e142dd88386295
Jonathan Bogdoll <jonathan.bogdoll@xbav.de>
2018-07-04 17:43:05 +0200

    Prompt: Datei/Ordner-Eingabe mit Autovervollständigung

--- Utilities/Utilities.Console/Prompt.cs
+++ Utilities/Utilities.Console/Prompt.cs
@@ -82,14 +82,102 @@ namespace Eulg.Utilities.Console
 
         public static string File(string prompt, bool mustExist = false, bool directoryMustExist = true, bool inline = false)
         {
-            return Show(prompt, s =>
+            var illegalChars = Path.GetInvalidPathChars();
+
+            while (true)
             {
-                var fn = Path.GetFullPath(s.Trim());
-                return Tuple.Create(fn, !string.IsNullOrWhiteSpace(fn)
-                                        && (!mustExist || System.IO.File.Exists(fn))
-                                        && (!directoryMustExist || Directory.Exists(Path.GetDirectoryName(fn)))
-                );
-            }, inline);
+                LeadIn();
+                if (inline)
+                {
+                    System.Console.Write(prompt);
+                    System.Console.Write(" ");
+                }
+                else
+                {
+                    System.Console.WriteLine(prompt);
+                    System.Console.Write("> ");
+                }
+
+                KeyValuePair<int, string[]> Autocomplete(string file)
+                {
+                    try
+                    {
+                        file = Path.GetFullPath(file);
+
+                        var allInFolder = Directory.Exists(file);
+                        var prefix = Directory.Exists(file) ? file : Path.GetDirectoryName(file);
+                        if (string.IsNullOrEmpty(prefix)) return new KeyValuePair<int, string[]>(0, new string[0]);
+
+                        return new KeyValuePair<int, string[]>(prefix.Length, Directory.EnumerateFileSystemEntries(prefix, allInFolder ? "*.*" : Path.GetFileName(file) + "*",
+                            SearchOption.TopDirectoryOnly).ToArray());
+                    }
+                    catch
+                    {
+                        return new KeyValuePair<int, string[]>(0, new string[0]);
+                    }
+                }
+
+                var input = ReadLine(c => !illegalChars.Contains(c), Autocomplete);
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                if (directoryMustExist && !Directory.Exists(Path.GetDirectoryName(input))) continue;
+                if (mustExist && !System.IO.File.Exists(input)) continue;
+
+                return input;
+            }
+        }
+
+        public static string Folder(string prompt, bool mustExist = false, bool inline = false)
+        {
+            var illegalChars = Path.GetInvalidPathChars();
+
+            while (true)
+            {
+                LeadIn();
+                if (inline)
+                {
+                    System.Console.Write(prompt);
+                    System.Console.Write(" ");
+                }
+                else
+                {
+                    System.Console.WriteLine(prompt);
+                    System.Console.Write("> ");
+                }
+
+                KeyValuePair<int, string[]> Autocomplete(string file)
+                {
+                    try
+                    {
+                        file = Path.GetFullPath(file);
+
+                        var allInFolder = Directory.Exists(file);
+                        var prefix = Directory.Exists(file) ? file : Path.GetDirectoryName(file);
+                        if (string.IsNullOrEmpty(prefix)) return new KeyValuePair<int, string[]>(0, new string[0]);
+
+                        return new KeyValuePair<int, string[]>(prefix.Length, Directory.EnumerateDirectories(prefix,
+                            allInFolder ? "*" : Path.GetFileName(file) + "*",
+                            SearchOption.TopDirectoryOnly).ToArray());
+                    }
+                    catch
+                    {
+                        return new KeyValuePair<int, string[]>(0, new string[0]);
+                    }
+                }
+
+                var input = ReadLine(c => !illegalChars.Contains(c), Autocomplete);
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                if (mustExist && !Directory.Exists(input)) continue;
+
+                return input;
+            }
         }
 
         public static string FileName(string prompt, string defaultName = null, string forceExtension = null)
@@ -103,7 +191,7 @@ namespace Eulg.Utilities.Console
                 System.Console.Write(prompt);
                 System.Console.Write(" ");
 
-                var input = FilteredReadString(null, c => !illegalChars.Contains(c));
+                var input = ReadLine(c => !illegalChars.Contains(c));
                 if (string.IsNullOrEmpty(input))
                 {
                     if (string.IsNullOrEmpty(defaultName))
@@ -170,67 +258,12 @@ namespace Eulg.Utilities.Console
         {
             if (inline)
             {
-                System.Console.WriteLine("{0} ({1})", prompt,
-                                  string.Join(" ", items.Select((i, n) => $"{n + 1}/{formatter(i).Key}")));
+                System.Console.WriteLine("{0} ({1})", prompt, string.Join(" ", items.Select((i, n) => $"{n + 1}/{formatter(i).Key}")));
             }
             else
             {
                 System.Console.WriteLine(prompt);
-
-                var digits = (int)Math.Ceiling(Math.Log10(items.Length));
-                var maxKeyLength = items.Max(x => formatter(x).Key.Length);
-
-                var indentation = (digits + 3) + (maxKeyLength + 2);
-                var maxDescLineLength = System.Console.WindowWidth - indentation;
-
-                var n = 0;
-                foreach (var item in items)
-                {
-                    var formatted = formatter(item);
-                    System.Console.Write("[{0}] {1}  ", (++n).ToString().PadLeft(digits), formatted.Key.PadRight(maxKeyLength));
-
-                    var lines = new List<string>();
-                    var desc = formatted.Value ?? string.Empty;
-                    while (desc.Length > 0)
-                    {
-                        if (desc.Length <= maxDescLineLength)
-                        {
-                            lines.Add(desc);
-                            break;
-                        }
-
-                        var lineBreak = desc.Substring(0, Math.Min(desc.Length, maxDescLineLength)).IndexOf(Environment.NewLine, StringComparison.Ordinal);
-                        var whitespace = desc.Substring(0, Math.Min(desc.Length, maxDescLineLength)).Select((c, i) => new KeyValuePair<int, char>(i + 1, c)).LastOrDefault(x => char.IsWhiteSpace(x.Value)).Key - 1;
-
-                        if (lineBreak >= 0)
-                        {
-                            lines.Add(desc.Substring(0, lineBreak));
-                            desc = desc.Substring(lineBreak + Environment.NewLine.Length);
-                        }
-                        else if (whitespace >= 0)
-                        {
-                            lines.Add(desc.Substring(0, whitespace));
-                            desc = desc.Substring(whitespace);
-                            while (char.IsWhiteSpace(desc[0]))
-                            {
-                                desc = desc.Substring(1);
-                            }
-                        }
-                    }
-
-                    if (lines.Count > 0)
-                    {
-                        System.Console.WriteLine(lines[0]);
-                        foreach (var line in lines.Skip(1))
-                        {
-                            System.Console.WriteLine(string.Empty.PadRight(indentation) + line);
-                        }
-                    }
-                    else
-                    {
-                        System.Console.WriteLine();
-                    }
-                }
+                FormatSelectChoices(items, formatter);
             }
 
             Tuple<int, bool> Parser(string input)
@@ -244,6 +277,126 @@ namespace Eulg.Utilities.Console
                 : Show(">", Parser, -1, true);
 
             return index == -1 ? blank : items[index];
+        }
+
+        public static T[] MultiEnum<T>(string prompt, bool inline = false, bool allowBlank = false, bool suppressDescription = false) where T : struct
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T");
+            }
+
+            var type = typeof(T);
+            var values = System.Enum.GetValues(type).Cast<T>().ToArray();
+            var displayValues = new Dictionary<T, KeyValuePair<string, string>>();
+            foreach (var value in values)
+            {
+                var name = value.ToString();
+                var desc = ((DescriptionAttribute)type.GetField(name).GetCustomAttribute(typeof(DescriptionAttribute)))?.Description;
+                displayValues.Add(value, new KeyValuePair<string, string>(name, desc == null || suppressDescription ? string.Empty : desc));
+            }
+
+            return MultiSelect(prompt, values, x => displayValues[x], inline, allowBlank);
+        }
+
+        public static T[] MultiSelect<T>(string prompt, T[] items, bool inline = false, bool allowBlank = false)
+        {
+            return MultiSelect(prompt, items, x => x.ToString(), inline, allowBlank);
+        }
+
+        public static T[] MultiSelect<T>(string prompt, T[] items, Func<T, string> formatter, bool inline = false, bool allowBlank = false)
+        {
+            return MultiSelect(prompt, items, x => new KeyValuePair<string, string>(formatter(x), string.Empty), inline, allowBlank);
+        }
+
+        public static T[] MultiSelect<T>(string prompt, T[] items, Func<T, KeyValuePair<string, string>> formatter, bool inline = false, bool allowBlank = false)
+        {
+            if (inline)
+            {
+                System.Console.WriteLine("{0} ({1})", prompt, string.Join(" ", items.Select((i, n) => $"{n + 1}/{formatter(i).Key}")));
+            }
+            else
+            {
+                System.Console.WriteLine(prompt);
+                FormatSelectChoices(items, formatter);
+            }
+
+            var keychars = System.Text.RegularExpressions.Regex.Escape(new string(items.SelectMany(i => formatter(i).Key).Concat("0123456789").Distinct().ToArray()));
+            var separator = new Regex($"[^{keychars}]+", RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.Singleline);
+
+            Tuple<int[], bool> Parser(string input)
+            {
+                var tokens = separator.Split(input)
+                    .Select(str => Enumerable.Range(0, items.Length).Cast<int?>().SingleOrDefault(i => formatter(items[i.Value]).Key.Equals(str, StringComparison.OrdinalIgnoreCase)) ?? (int.Parse(input) - 1))
+                    .Distinct()
+                    .ToArray();
+
+                return Tuple.Create(tokens, tokens.All(t => t >= 0 && t < items.Length));
+            }
+
+            var index = !allowBlank
+                ? Show(">", Parser, true)
+                : Show(">", Parser, new int[0], true);
+
+            return Array.ConvertAll(index, i => items[i]);
+        }
+
+        private static void FormatSelectChoices<T>(T[] items, Func<T, KeyValuePair<string, string>> formatter)
+        {
+            var digits = (int)Math.Ceiling(Math.Log10(items.Length));
+            var maxKeyLength = items.Max(x => formatter(x).Key.Length);
+
+            var indentation = (digits + 3) + (maxKeyLength + 2);
+            var maxDescLineLength = System.Console.WindowWidth - indentation;
+
+            var n = 0;
+            foreach (var item in items)
+            {
+                var formatted = formatter(item);
+                System.Console.Write("[{0}] {1}  ", (++n).ToString().PadLeft(digits), formatted.Key.PadRight(maxKeyLength));
+
+                var lines = new List<string>();
+                var desc = formatted.Value ?? string.Empty;
+                while (desc.Length > 0)
+                {
+                    if (desc.Length <= maxDescLineLength)
+                    {
+                        lines.Add(desc);
+                        break;
+                    }
+
+                    var lineBreak = desc.Substring(0, Math.Min(desc.Length, maxDescLineLength)).IndexOf(Environment.NewLine, StringComparison.Ordinal);
+                    var whitespace = desc.Substring(0, Math.Min(desc.Length, maxDescLineLength)).Select((c, i) => new KeyValuePair<int, char>(i + 1, c)).LastOrDefault(x => char.IsWhiteSpace(x.Value)).Key - 1;
+
+                    if (lineBreak >= 0)
+                    {
+                        lines.Add(desc.Substring(0, lineBreak));
+                        desc = desc.Substring(lineBreak + Environment.NewLine.Length);
+                    }
+                    else if (whitespace >= 0)
+                    {
+                        lines.Add(desc.Substring(0, whitespace));
+                        desc = desc.Substring(whitespace);
+                        while (char.IsWhiteSpace(desc[0]))
+                        {
+                            desc = desc.Substring(1);
+                        }
+                    }
+                }
+
+                if (lines.Count > 0)
+                {
+                    System.Console.WriteLine(lines[0]);
+                    foreach (var line in lines.Skip(1))
+                    {
+                        System.Console.WriteLine(string.Empty.PadRight(indentation) + line);
+                    }
+                }
+                else
+                {
+                    System.Console.WriteLine();
+                }
+            }
         }
 
         public static T Keystroke<T>(string prompt, Func<char, Tuple<T, bool>> parser)
@@ -295,12 +448,14 @@ namespace Eulg.Utilities.Console
         {
             LeadIn();
             System.Console.Write(prompt);
-            return FilteredReadString('*', _ => true);
+            return ReadLine(_ => true, mask: '*');
         }
 
-        private static string FilteredReadString(char? mask, Func<char, bool> filter)
+        private static string ReadLine(Func<char, bool> filter, Func<string, KeyValuePair<int, string[]>> autocomplete = null, char? mask = null)
         {
             var chars = new List<char>();
+            var lastTab = DateTime.Now.AddDays(-1);
+
             while(true)
             {
                 var ck = System.Console.ReadKey(true);
@@ -319,6 +474,34 @@ namespace Eulg.Utilities.Console
                             chars.RemoveAt(chars.Count - 1);
                         }
                         break;
+                    case ConsoleKey.Tab:
+                        if (autocomplete == null) break;
+                        var suggestions = autocomplete(new string(chars.ToArray()));
+                        switch (suggestions.Value.Length)
+                        {
+                            case 0:
+                                System.Console.Beep();
+                                break;
+                            case 1:
+                                System.Console.CursorLeft -= chars.Count;
+                                System.Console.Write(suggestions.Value[0]);
+                                chars = suggestions.Value[0].ToList();
+                                break;
+                            default:
+                                var now = DateTime.Now;
+                                if ((now - lastTab).TotalMilliseconds > 300)
+                                {
+                                    lastTab = now;
+                                    break;
+                                }
+
+                                System.Console.WriteLine();
+                                ShowAutocompleteSuggestions(Array.ConvertAll(suggestions.Value, _ => _.Substring(suggestions.Key)));
+                                System.Console.WriteLine();
+                                System.Console.Write(new string(chars.ToArray()));
+                                break;
+                        }
+                        break;
                     default:
                         if (filter(ck.KeyChar))
                         {
@@ -331,6 +514,39 @@ namespace Eulg.Utilities.Console
                         }
                         break;
                 }
+            }
+        }
+
+        private static void ShowAutocompleteSuggestions(string[] suggestions)
+        {
+            const int minSpace = 3;
+
+            var numberOfColumns = Enumerable.Range(1, suggestions.Length).Last(n =>
+                GetPartitions(suggestions, (suggestions.Length / n) + (suggestions.Length % n))
+                    .Select(p => p.Max(_ => _.Length)).Sum() + minSpace * (n - 1) < System.Console.WindowWidth);
+
+            var columns = GetPartitions(suggestions,
+                (suggestions.Length / numberOfColumns) + (suggestions.Length % numberOfColumns)).ToArray();
+            var columnWidths = columns.Select(c => c.Max(_ => _.Length)).ToArray();
+
+            for (var r = 0; r < columns[0].Length; ++r)
+            {
+                var columnCount = columns.Count(c => r < c.Length);
+                for (var c = 0; c < columnCount; ++c)
+                {
+                    System.Console.Write(columns[c][r].PadRight(columnWidths[c]));
+                    System.Console.Write(c == columnCount - 1
+                        ? Environment.NewLine
+                        : string.Empty.PadRight(minSpace));
+                }
+            }
+        }
+
+        private static IEnumerable<string[]> GetPartitions(IReadOnlyList<string> items, int partitionSize)
+        {
+            for (var n = 0; n * partitionSize < items.Count; ++n)
+            {
+                yield return Enumerable.Range(n * partitionSize, partitionSize).Where(_ => _ < items.Count).Select(_ => items[_]).ToArray();
             }
         }
 
